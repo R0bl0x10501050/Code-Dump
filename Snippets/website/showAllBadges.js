71e5410d8b456052524f3f0b8696785826a0db75
R0bl0x10501050 <74582614+R0bl0x10501050@users.noreply.github.com>
2021-08-10 17:16:22 -0700

    Update showAllBadges.js

--- Snippets/website/showAllBadges.js
+++ Snippets/website/showAllBadges.js
@@ -1,10 +1,11 @@
 // How To Show ALL Badges Of A Game On The Site
 
 let secondsToRunFor = 25 /* Change this number if you want */
+let timesASecond = 4 /* Change this number to the number of times a second to try to click the button */
 let clicked = 0
 let interval
 interval = setInterval(function() {
     if (clicked == secondsToRunFor*4) { clearInterval(interval) }
     clicked++
     document.querySelector(`button.btn-full-width[ng-click="$ctrl.seeMore()"]`).click()
-}, 250)
+}, 1000/timesASecond)
