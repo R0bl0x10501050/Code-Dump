7af41fe6678868b14edcfef3b36dc776da7557d9
R0bl0x10501050 <74582614+R0bl0x10501050@users.noreply.github.com>
2021-08-10 23:53:55 -0700

    Create getDictionaryValueParent.lua

---
+++ Snippets/getDictionaryValueParent.lua
@@ -0,0 +1,22 @@
+local find
+find = function(originalTable, findValue)
+	for index, wrappedTable in pairs(originalTable) do
+		if wrappedTable == findValue then
+			return wrappedTable
+		else
+			return nil
+		end
+	end
+end
+
+local recursiveLoop
+recursiveLoop = function(parent)
+	for k, v in pairs(parent) do
+		if type(v) ~= 'table' then continue end
+		if find(v, tagVal) then
+			return v
+		else
+			return recursiveLoop(v)
+		end
+	end
+end
